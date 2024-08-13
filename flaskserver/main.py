dd7ff0d196cc9e96e9eb79d253137c223b3a9236
Arjun Dabir <arjunadabir@gmail.com>
2024-08-12 20:18:58 -0700

    feat:basic flask server logic

--- flaskserver/main.py
+++ flaskserver/main.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, make_response, request, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return ""
+
+
+@app.route('/sasi-team-members')
+def sasi_team_members():
+    response = make_response("sasi members!")
+    response.set_cookie("admin", "True")
+    return response
+
+
+@app.route("/<slug>")
+def qr_code_redirect(slug):
+    admin = request.cookies.get("admin")
+    if admin:
+        return "admin member!"
+    else:
+        return redirect("https://wp.ovptl.uci.edu/sasi/")
 
 
 if __name__ == "__main__":
