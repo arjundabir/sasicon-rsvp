97b778539a93e332f88fc4582d892e57779486a4
Arjun Dabir <arjunadabir@gmail.com>
2024-08-12 20:21:28 -0700

    chore: fixed pylint errors on flaskserver main.py

--- flaskserver/main.py
+++ flaskserver/main.py
@@ -1,3 +1,6 @@
+"""
+This is the main file for the flask server.
+"""
 from flask import Flask, make_response, request, redirect
 
 app = Flask(__name__)
@@ -5,11 +8,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    """
+    Return the home page
+    """
     return ""
 
 
 @app.route('/sasi-team-members')
 def sasi_team_members():
+    """
+    Set the cookie to "True" and redirect to the sasi team members page
+    """
     response = make_response("sasi members!")
     response.set_cookie("admin", "True")
     return response
@@ -17,12 +26,22 @@ def sasi_team_members():
 
 @app.route("/<slug>")
 def qr_code_redirect(slug):
+    """
+    Redirect to the sasi team members page if the admin cookie is set
+    """
     admin = request.cookies.get("admin")
     if admin:
-        return "admin member!"
+        return handle_slug(slug)
     else:
         return redirect("https://wp.ovptl.uci.edu/sasi/")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+def handle_slug(slug):
+    """
+    Handle the slug and return the appropriate response
+    """
+    return slug
